0f119074714e674a51f73dfbc05e420ed03d0487
Xanday <xandaychannel@gmail.com>
2024-04-25 23:31:25 +0300

    deploy3

--- src/app/404/404.component.ts
+++ src/app/404/404.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-404',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './404.component.html',
   styleUrl: './404.component.scss'
 })
